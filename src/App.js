e82c4d19a3197504b0cc9ae12c8f4216500b3c6c
AquinoToy <salinas.aquino.omarivan@gmail.com>
2023-11-05 21:08:30 -0600

    Login

--- src/App.js
+++ src/App.js
@@ -1,109 +1,55 @@
-import './App.css';
-import 'https://kit.fontawesome.com/41bcea2ae3.js';
+const express = require('express');
+const {engine} = require('express-handlebars');
+const myconnection = require('express-myconnection');
+const mysql = require('mysql2');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 
-function App() {
-  return (
-    <div>
-      <header>
-        <div className="container__menu">
-          <div className="menu">
-            <input type="checkbox" id="check__menu" />
-            <label htmlFor="check__menu" id="label__check">
-              <i className="fas fa-bars icon__menu"></i>
-            </label>
-            <nav>
-              <ul>
-                <li><a href="#" id="selected"></a></li>
-                <li>
-                  <a href="#">Otras opciones</a>
-                  <ul>
-                    <li><a href="#">Perfil</a></li>
-                    <li><a href="#">Ajustes</a></li>
-                    <li><a href="#">Red</a></li>
-                    <li><a href="#">Mensajes</a></li>
-                    <li><a href="#">Políticas de uso</a></li>
-                  </ul>
-                </li>
-                <li><a href="CuidadoCultivos.html">Cuidado de cultivos</a></li>
-                <li><a href="SeleccionArtificial.html">Seleccion artificial</a></li>
-                <li><a href="Recursos.html">Recursos</a></li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-      <main className="main">
-        <section className="container">
-          <h2 className="title_section">Bienvenidos a Adgamus</h2>
-          <p className="copy">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          <div className="enfoques">
-            <div className="enfoque" id="enfoque1">
-              <label>
-                <h3><em>Plantas</em></h3>
-              </label>
-            </div>
-            <div className="enfoque" id="enfoque2">
-              <label>
-                <h3><em>Animales</em></h3>
-              </label>
-            </div>
-            <div className="enfoque" id="enfoque3">
-              <label>
-                <h3><em>Recursos</em></h3>
-              </label>
-            </div>
-          </div>
-        </section>
-        <section className="Informacion_adgamus">
-          <article>
-            <h2>¿Cuál es nuestro objetivo?</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio est nobis consectetur officia dolore accusamus sunt vel quo exercitationem doloribus dolores repudiandae atque, cupiditate laboriosam id, accusantium ducimus tempore ullam?</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque porro nostrum eveniet cumque animi totam error placeat quos molestiae non, aperiam incidunt ratione ipsum. Illum ducimus aliquam, mollitia dicta nulla at et! Alias et facere odio esse consequatur sunt dolorum?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iusto eligendi nesciunt soluta molestias sit.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis et eum reiciendis unde dignissimos temporibus ipsa suscipit animi expedita, praesentium recusandae minima quasi mollitia culpa fugiat odit voluptate rerum incidunt.</p>
-          </article>
-          <article>
-            <h2>¿Qué pretendemos lograr?</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio est nobis consectetur officia dolore accusamus sunt vel quo exercitationem doloribus dolores repudiandae atque, cupiditate laboriosam id, accusantium ducimus tempore ullam?</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque porro nostrum eveniet cumque animi totam error placeat quos molestiae non, aperiam incidunt ratione ipsum. Illum ducimus aliquam, mollitia dicta nulla at et! Alias et facere odio esse consequatur sunt dolorum?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iusto eligendi nesciunt soluta molestias sit.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis et eum reiciendis unde dignissimos temporibus ipsa suscipit animi expedita, praesentium recusandae minima quasi mollitia culpa fugiat odit voluptate rerum incidunt.</p>
-          </article>
-          <article>
-            <h2>Visión</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio est nobis consectetur officia dolore accusamus sunt vel quo exercitationem doloribus dolores repudiandae atque, cupiditate laboriosam id, accusantium ducimus tempore ullam?</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque porro nostrum eveniet cumque animi totam error placeat quos molestiae non, aperiam incidunt ratione ipsum. Illum ducimus aliquam, mollitia dicta nulla at et! Alias et facere odio esse consequatur sunt dolorum?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iusto eligendi nesciunt soluta molestias sit.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis et eum reiciendis unde dignissimos temporibus ipsa suscipit animi expedita, praesentium recusandae minima quasi mollitia culpa fugiat odit voluptate rerum incidunt.</p>
-          </article>
-          <article>
-            <h2>Misión</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio est nobis consectetur officia dolore accusamus sunt vel quo exercitationem doloribus dolores repudiandae atque, cupiditate laboriosam id, accusantium ducimus tempore ullam?</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque porro nostrum eveniet cumque animi totam error placeat quos molestiae non, aperiam incidunt ratione ipsum. Illum ducimus aliquam, mollitia dicta nulla at et! Alias et facere odio esse consequatur sunt dolorum?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iusto eligendi nesciunt soluta molestias sit.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis et eum reiciendis unde dignissimos temporibus ipsa suscipit animi expedita, praesentium recusandae minima quasi mollitia culpa fugiat odit voluptate rerum incidunt.</p>
-          </article>
-        </section>
-      </main>
-      <footer className="footer">
-        <div className="containerF">
-          <div className="title_section">
-            <h2>Contáctenos</h2>
-          </div>
-          <div className="redes_sociales">
-            <a href="#"><i className="fa-brands fa-facebook"></i></a>
-            <a href="#"><i className="fa-brands fa-instagram"></i></a>
-            <a href="#"><i className="fa-solid fa-envelope"></i></a>
-          </div>
-          <div className="containerF">
-            <p className="copy">
-              2023 Adgamus / Todos los derechos reservados
-            </p>
-          </div>
-        </div>
-      </footer>
-    </div>
-  );
-}
+const loginRoutes = require('./routes/login');
 
-export default App;
+const app = express();
+app.set('port', 4000);
+
+app.set('views', __dirname + '/views');
+app.engine('.hbs', engine({
+extname: '.hbs',
+}));
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+app.use(bodyParser.json());
+
+app.use(myconnection(mysql,{
+    host: 'localhost',
+    user: 'root',
+    password: 'n0m3l0',
+    port: '3308',
+    database:'adgamus'
+}));
+
+app.use(session({ 
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.listen(app.get('port'), () => {
+    console.log('Listening on port', app.get('port'));
+});
+
+app.use('/', loginRoutes);
+
+app.get('/', (req, res) => {
+
+    if(req.session.loggedin == true){
+
+        res.render('home', {name: req.session.name});
+
+    } else{
+
+        res.redirect('/login');
+
+    }
+});
---
+++ src/controllers/LoginController.js
@@ -0,0 +1,99 @@
+const bcrypt = require('bcrypt');
+
+function login(req, res){
+
+    if(req.session.loggedin != true){
+
+        res.render('login/index');
+
+    } else{
+
+        res.redirect('/');
+    }
+}
+
+function auth(req, res){
+    const data = req.body;
+   
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM usuario WHERE idUsuario = ?', [data.idUsuario], (err, userdata) =>{
+            if(userdata.length > 0){
+                userdata.forEach(element => {
+
+                    bcrypt.compare(data.Contraseña, element.Contraseña, (err, isMatch) => {
+
+                        if(!isMatch){
+                            res.render('login/index', {error: 'Error: !Contraseña incorrecta!'});
+                        }
+                        else{
+                            req.session.loggedin = true;
+                            req.session.name = element.NombreUsuario;
+
+                            res.redirect('/');
+                        }
+                    });
+
+                });
+
+            } else{
+                res.render('login/index', {error: 'Error: ¡Usuario no existente!'});
+            }
+        })
+    });
+}
+
+function register(req, res){
+
+    if(req.session.loggedin != true){
+
+        res.render('login/register');
+
+    } else{
+
+        res.redirect('/');
+    }
+}
+
+function storeUser(req,res){
+    const data = req.body;
+
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM usuario WHERE idUsuario = ?', [data.idUsuario], (err, userdata) =>{
+            if(userdata.length > 0){
+                res.render('login/register', {error: 'Error: ¡Usuario ya existente!'});
+            } else{
+
+                bcrypt.hash(data.Contraseña, 12).then(hash =>{
+                    data.Contraseña = hash;
+            
+                    req.getConnection((err, conn) => {
+                        conn.query('INSERT INTO usuario SET ?', [data], (err, rows) => {
+
+                            req.session.loggedin = true;
+                            req.session.name = data.NombreUsuario;
+
+                            res.redirect('/');
+                        });
+                    });
+                });
+            }
+        })
+    });
+}
+
+function logout(req, res){
+    if(req.session.loggedin == true){
+
+        req.session.destroy();
+    }
+        res.redirect('/login');
+    
+}
+
+module.exports = {
+    login,
+    register,
+    storeUser,
+    auth,
+    logout,  
+}
