877abe53dff93c9a3624076c6bd3fc9de6f6fcf3
GVIM0 <guimo.enterprise@gmail.com>
2023-11-10 10:44:40 -0600

    Routes

--- src/routes/login.js
+++ src/routes/login.js
@@ -3,11 +3,11 @@ const LoginController = require('../controllers/LoginController');
 
 const router = express.Router();
 
-router.get('/login', LoginController.login);
-router.post('/login', LoginController.auth);
-router.get('/register', LoginController.register);
-router.post('/register', LoginController.storeUser);
-router.get('/logout', LoginController.logout);
+router.get('/Adgamus/login', LoginController.login);
+router.post('/Adgamus/login', LoginController.auth);
+router.get('/Adgamus/register', LoginController.register);
+router.post('/Adgamus/register', LoginController.storeUser);
+router.get('/Adgamus/logout', LoginController.logout);
 
 
 module.exports = router;
