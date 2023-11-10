47b1a8da42fae31fea15ab4716af87d4fc982eb6
Guimo Company <144580107+GVIM0@users.noreply.github.com>
2023-11-10 17:02:40 -0600

    Update login.js

--- src/routes/login.js
+++ src/routes/login.js
@@ -3,7 +3,7 @@ const LoginController = require('../controllers/LoginController');
 
 const router = express.Router();
 
-router.get('/Adgamus/login/', LoginController.login);
+router.get('/Adgamus/login', LoginController.login);
 router.post('/Adgamus/login', LoginController.auth);
 router.get('/Adgamus/register', LoginController.register);
 router.post('/Adgamus/register', LoginController.storeUser);
